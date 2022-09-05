7ecb364d25756c22e7ba837259f3e622855b33e3
Aditya <63107098+Aditya0721@users.noreply.github.com>
2022-09-05 10:51:08 +0530

    routing using Router object

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const app = express();
+const router = require('./routes/route')
 
-// routing using application object
-app.get('/',(req, res)=>{
-    res.send("Welcome")
-})
-app.get('/about',(req, res)=>{
-    res.send("About Page")
-})
-
+// // routing using application object
+// app.get('/',(req, res)=>{
+//     res.send("Welcome")
+// })
+// app.get('/about',(req, res)=>{
+//     res.send("About Page")
+// })
+app.use('/', router)
 app.listen(3000,()=>{
     console.log("express tutorial")
 })
