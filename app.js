4f20750f5b4b4667e02acc805dc60ee62c0358d4
Aditya <63107098+Aditya0721@users.noreply.github.com>
2022-09-05 10:45:44 +0530

    routing using application object

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express();
 
+// routing using application object
+app.get('/',(req, res)=>{
+    res.send("Welcome")
+})
+app.get('/about',(req, res)=>{
+    res.send("About Page")
+})
+
 app.listen(3000,()=>{
     console.log("express tutorial")
 })
